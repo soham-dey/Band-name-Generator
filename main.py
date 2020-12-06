af9cc0fe5512c3267c9b6b2f242d3a415c8d1a3c
SOHAM DEY <deysoham.1999@gmail.com>
2020-12-06 08:34:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hi, Welcome to the Band Name Generator!\n")
+city_name=input("Enter the name of the city where you grew up:\n")
+pet_name=input("Enter your favourite pet's name:\n")
+print("Your Band name is: " + city_name +" " + pet_name + "s")
